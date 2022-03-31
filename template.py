f2a269d9d3e6626c9a307cc5cc90fa28abe2c901
kenkn <skt7tp@gmail.com>
2022-03-31 19:12:41 +0900

    initial commit

---
+++ template.py
@@ -0,0 +1,10 @@
+# ===========================
+#       tenkei 90 #~~~
+# Author: kenkn(Sawada Kenta)
+# ===========================
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
